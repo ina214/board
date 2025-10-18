985b273a7bc744c9a9ad8121fb6f432390cbb0a3
hyesu <ina214@naver.com>
2025-10-18 12:37:58 +0900

    [1018] 삭제

--- pkc/board/src/main/java/com/example/board/BoardDto.java
+++
@@ -1,9 +0,0 @@
-package com.example.board;
-
-import lombok.Data;
-
-@Data
-public class BoardDto {
-  private int id, sung, juso, readnum;
-  private String title, name, pwd, content, hobby, writeday;
-}
